8c6163b293907948518472bbde0b974b79db19c2
Tomas-Pierce-at-UNC <ttomas@email.unc.edu>
2022-03-02 11:15:37 -0500

    improving C-interop

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,5 @@
 
-use std::env;
-use std::io;
-
-mod head;
-mod take;
-mod cine;
-
-
-
 fn main() {
-    let args = env::args();
-    for arg in args {
-        if arg.ends_with(".cine") {
-            match run(&arg) {
-                Ok(_) => {println!("processed {} successfully", arg);},
-                Err(e) => {println!("processing {} failed with error: {}", arg, e);},
-            };
-        }
-    }
-}
+    println!("hello world");
 
-fn run(cine_name :&String) -> io::Result<()> {
-    let mut stepper = cine::FrameStepper::new(cine_name)?;
-    while stepper.has_next() {
-        let data = stepper.next()?;
-    }
-    Ok(())
 }
