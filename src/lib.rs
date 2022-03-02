478a5f8d8c8f0d2efd1608e3f900efaa2333f306
Tomas-Pierce-at-UNC <ttomas@email.unc.edu>
2022-03-02 10:06:57 -0500

    upload so far

--- src/lib.rs
+++ src/lib.rs
@@ -5,21 +5,21 @@ pub mod cine;
 
 use std::ffi;
 use std::ptr;
-use std::mem;
-
-/* I know perfectly well I should be shot for this.
- * I'm doing it anyway because I need to be able to
- * get the results from Python 3, and this seemed
- * like the easiest way to do it.
- */
 
 
 #[no_mangle]
-pub unsafe extern "C" fn get_image_i(name :*const libc::c_char, index :u64, size_out :*mut u64) -> * const u8 {
+pub unsafe extern "C" fn get_image_size(name :*const libc::c_char) -> u64 {
+    let fname = ffi::CStr::from_ptr(name);
+    let filename = fname.to_str().unwrap().to_owned();
+    let stepper = cine::FrameStepper::new(&filename).unwrap();
+    stepper.bytes_count()
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn get_image_i(name :*const libc::c_char, index :u64,) -> * const u8 {
     let fname = ffi::CStr::from_ptr(name);
     let filename = fname.to_str().unwrap().to_owned();
     let mut stepper = cine::FrameStepper::new(&filename).unwrap();
-    *size_out = stepper.bytes_count();
     let mut thing = vec![0u8;1];
     for _i in 0..index {
         if stepper.has_next() {
@@ -28,5 +28,8 @@ pub unsafe extern "C" fn get_image_i(name :*const libc::c_char, index :u64, size
             return ptr::null();
         }
     }
+    /* effectively null terminates */
+    thing.push(0u8);
+    /* internal pointer can be treated as byte string */
     thing.as_ptr()
 }
